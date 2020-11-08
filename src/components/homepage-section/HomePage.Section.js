1aa88e452727de320a76d51858fe90bcbb9e1a9b
= <nassiromid@gmail.com>
2020-11-08 14:29:28 -0800

    Added Home Page wth 2 section products

---
+++ src/components/homepage-section/HomePage.Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomePageSection.css';
+
+const HomePageSection = ({ title, imageUrl }) => {
+    console.log(imageUrl);
+    return(
+        <div className="homePageSection">
+            <div className="homePageSection__pic">
+                <img src={imageUrl} alt="product section"/>
+            </div>
+            <div className="homePageSection__title">
+                <h5>{title}</h5>
+            </div>
+        </div>
+    )
+}
+
+export default HomePageSection;
