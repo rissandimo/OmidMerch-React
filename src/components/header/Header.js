6d1ee6d86b492ce537a27030c7a4d397cd31ce2b
= <nassiromid@gmail.com>
2020-11-08 13:53:33 -0800

    Added cart icon to Header and styled header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './header.css';
 
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component{
@@ -8,9 +10,17 @@ class Header extends React.Component{
     render(){
         return(
             <div className="header">
+
+            <div className="header__links">
                 <Link to='/register'>Register</Link>
                 <Link to='/login'>Login</Link>
                 <Link to='/shop'>Shop</Link>
+            </div>
+
+            <div className="header__cart">
+                <ShoppingCartIcon fontSize='large'/>
+            </div>
+
             </div>
         )
     }
