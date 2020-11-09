fdeffabaec27b14caaa3ad83d2374194dd3d2b35
= <nassiromid@gmail.com>
2020-11-09 09:12:42 -0800

    Updated layout for PreviewItem

--- src/components/preview-item/Product.js
+++ src/components/preview-item/Product.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './product.css';
 
 const PreviewItem = ({ item }) => {
 
     const { name, imageUrl, price } = item;
     return (
         <div className="previewItem">
+        <div className="previewItem_description">
             <p>{name}</p>
-            <img src={imageUrl} alt=""/>
             <h4>${price}</h4>
+        </div>
+            <img src={imageUrl} alt=""/>
         </div>
     )
 }
