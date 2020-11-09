370680272b140d944d20c42c020eb3cdda98f5f1
= <nassiromid@gmail.com>
2020-11-08 17:27:38 -0800

    Added login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import './login.css';
+
+import { Button, FormControl, TextField } from '@material-ui/core';
 
 class Login extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+
+        this.setState({
+            email: '',
+            password: ''
+        })
+    }
+
+
     render(){
+
         return(
             <div className="login">
-                Login
+                <form onSubmit={this.handleSubmit} className='login__form'>
+                    <FormControl>
+                        <TextField value={this.state.email} label='Email' name='email' onChange={this.handleChange} />
+                    </FormControl>
+                    <FormControl>
+                        <TextField value={this.state.password} label='Password' name='password' type='password' onChange={this.handleChange} />
+                    </FormControl>
+                    <Button color='primary' type='submit'>Login</Button>
+                </form>
             </div>
         )
     }
