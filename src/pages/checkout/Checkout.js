6a0b91a368ebc3441d5c3d9af3e94d788bf5831f
Omid Nassir <nassiromid@gmail.com>
2020-12-06 10:58:26 -0800

    Able to retrieve cart items

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({ cartItems }) => (
     <div className="checkoutPage">
-        checkout page
+        {cartItems.map(item => (
+            <p>{item.name}</p>
+        ))}
     </div>
-)
+);
 
-export default CheckoutPage;
+const mapStateToProps = state => ({
+    cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
