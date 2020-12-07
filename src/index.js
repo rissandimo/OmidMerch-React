ba77e2fba5f031e0ef2c96df416fea2695546dd9
Omid Nassir <nassiromid@gmail.com>
2020-12-07 10:32:42 -0800

    Able to persist storage using local storage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 // Redux
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
