3bf8602a4e5bde3c708abb364198f47a2a586bbf
Omid Nassir <nassiromid@gmail.com>
2020-12-07 10:33:46 -0800

    Added comments

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,16 +2,14 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-
 import userReducer from '../redux/user/user-reducer';
 import cartReducer from '../redux/cart/cart-reducer';
 
 const persistConfig = {
     key: 'root',
-    storage,
-    whitelist: ['cart']
-  };
-  
+    storage, // use local storage
+    whitelist: ['cart'] // persist cart reducer, user persisted with firebase
+};
 
 const rootReducer = combineReducers({
     cart: cartReducer,
