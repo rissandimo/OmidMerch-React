93b394dbfdcd47756d26a8fd682300d366395b94
Omid Nassir <nassiromid@gmail.com>
2020-12-11 16:07:59 -0800

    Able to render collection based on url param

--- src/pages/category/CategoryPage.js
+++ src/pages/category/CategoryPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './categoryPage.css';
 
-import PreviewItem from '../../components/preview-item/PreviewItem';
-import { render } from '@testing-library/react';
+import { connect } from 'react-redux';
 
-const CategoryPage = ({ match }) => {
-    console.log(match);
+import { selectCollection } from '../../redux/shop/shop-selector';
+
+import PreviewItem from '../../components/preview-item/PreviewItem';
+
+const CategoryPage = ({ collection }) => {
+    console.log(collection);
     return(
         <div className="categoryPage">
             <h2>Category Page</h2>
@@ -13,4 +16,8 @@ const CategoryPage = ({ match }) => {
     )
 }
 
-export default CategoryPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+  });
+
+export default connect(mapStateToProps)(CategoryPage);
--- src/redux/shop/shop-selector.js
+++ src/redux/shop/shop-selector.js
@@ -2,7 +2,20 @@ import { createSelector } from 'reselect';
 
 const shopSelector = state => state.shop;
 
+const COLLECTION_ID_MAP = {
+    womens: 1,
+    localPickup: 2,
+    housewares: 3,
+    jewelry: 4
+}
+
 export const getCollectionItems = createSelector(
     [shopSelector],
     shop => shop.collections
 )
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [getCollectionItems],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+  );
