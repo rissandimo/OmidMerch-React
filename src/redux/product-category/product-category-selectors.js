ba0d65ae0df7bf7ee13d5af4b81d32eca10c4667
Omid Nassir <nassiromid@gmail.com>
2021-01-09 16:19:33 -0800

    Moved all client code to client dir

--- src/redux/product-category/product-category-selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import productCategoryReducer from './product-category-reducer';
-
-const getProductCateogory = state => state.productCategory;
-
-export const getProductCategorySection = createSelector(
-    [getProductCateogory],
-    category => category.sections
-);
