cc06725fd20d9029693f465af0cdb693d458b085
Omid Nassir <nassiromid@gmail.com>
2021-01-10 19:40:29 -0800

    Cleaned code

--- client/src/components/stripe-button/stripeButton.js
+++ client/src/components/stripe-button/stripeButton.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
-
-// import { connect } from 'react-redux';
-
 import axios from 'axios';
-// import { clearAllItemsFromCart } from '../../redux/cart/cart-actions';
 
 const StripeCheckoutButton = ({ price }) => {
 
     const priceForStripe = price * 100;
-    // const stripePublishableKey = 'pk_test_51HTuCLGlap3A7zAIyQX1bSHtbMDjIE7DuGETSigZyL4WlBsL8pRBFwqXd6yDmoPaiQNf9ftJPoRxEy6JQTxxTDSe00Kw0mqb8d';
-    const stripePublishableKey = 'pk_live_51HTuCLGlap3A7zAIhzAXSb0iF8H5Dfiiun5H4PiZaQFfRPI1DCmm36yA1lGdHfjWf7VmGOu2kg4j3F17Euy1HL8U00aDTAEYeg';
 
     const onToken = token => {
         console.log(token);
@@ -24,7 +18,6 @@ const StripeCheckoutButton = ({ price }) => {
             }
         }).then(response => {
             alert('Payment Successful');
-            // clearAllItemsFromCart();
         }).catch(error => {
             console.log('Payment error: ', JSON.parse(error));
             alert('This were an issue with your payment. Please make sure you use the provided credit card');
@@ -42,15 +35,11 @@ const StripeCheckoutButton = ({ price }) => {
                 name="Omid Merch"
                 panelLabel="Pay Now"
                 shippingAddress
-                stripeKey={stripePublishableKey}
+                stripeKey={process.env.STRIPE_SECRET_KEY_LIVE}
                 token={onToken} 
             />
         </div>
     )
 }
-
-// const mapDispatchToProps = dispatch => ({
-//     clearAllItemsFromCart: dispatch(clearAllItemsFromCart())
-// })
 
 export default StripeCheckoutButton;
